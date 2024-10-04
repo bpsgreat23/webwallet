d422d69046d5d47e09af35953b9be4d409219aea
bpsgreat23 <96990602+bpsgreat23@users.noreply.github.com>
2024-10-04 22:25:18 +0530

    Update ethWallet.jsx

--- wallet/src/ethWallet.jsx
+++ wallet/src/ethWallet.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { ethers } from 'ethers';
+import { HDNodeWallet,ethers, Wallet } from 'ethers';
+import { mnemonicToSeed } from "bip39";
+// import{HDNode} from '@scure/bip32';
 
 const EthWallet = ({ mnemonic }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -7,9 +9,22 @@ const EthWallet = ({ mnemonic }) => {
 
   const addWallet = async () => {
     try {
-      const hdNode = ethers.HDNodeWallet.fromPhrase(mnemonic);
+      // const hdNode = ethers.HDNodeWallet.fromPhrase(mnemonic);
+      // const derivationPath = `m/44'/60'/${currentIndex}'/0'`;
+
+      const seed = await mnemonicToSeed(mnemonic);
       const derivationPath = `m/44'/60'/${currentIndex}'/0'`;
-      const wallet = hdNode.derivePath(derivationPath);
+      const hdNode = HDNodeWallet.fromSeed(seed);
+      const child = hdNode.derivePath(derivationPath);
+      const privateKey = child.privateKey;
+      const wallet = new Wallet(privateKey);
+
+
+      // const hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic);
+      
+
+      // const derivationPath = `m/44'/60'/0'/0/${currentIndex}`;
+      // const wallet = hdNode.derivePath(derivationPath);
 
       setCurrentIndex(prevIndex => prevIndex + 1);
       setAddresses(prevAddresses => [...prevAddresses, wallet.address]);
